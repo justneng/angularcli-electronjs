f5f39e715f3c8291fa2e3516dfe92243d9961d00
neng <wanchana@geniustree.co.th>
2019-06-15 19:51:17 +0700

    add path service

--- src/app/provider/path.service.ts
+++ src/app/provider/path.service.ts
@@ -14,6 +14,9 @@ export class PathService {
     if(serve_mode) {
       return path.join('../../../assets/', p);
     } else {
+      //กรณีที่ run แบบ build electron [local, linux, windows, mac] ไฟล์ path จะถูก pack ไปที่ <<APP_ROOT_PATH>>/dist
+      //และ folder assets เดิมที่อยู่ภายใต้ <<APP_ROOT_PATH>>/src/assets จะถูกย้ายไปที่ <<APP_ROOT_PATH>>/dist/assets
+      //ทำให้ในการ dev และ release จะได้ที่อยู่ของ folder ต่างกัน
       return path.join('assets/', p);
     }
   }
